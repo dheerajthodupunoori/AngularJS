1c49044edf3037f77bf6fd8fc8a6fe163193c787
dheerajthodupunoori <dheeraj.thodupunoori01@gmail.com>
2018-12-05 10:57:25 +0530

    Add files via upload

---
+++ StudentService.asmx.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Web.Script.Serialization;
+
+namespace AngularDemo
+{
+    /// <summary>
+    /// Summary description for StudentService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class StudentService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void GetAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+            SqlConnection con = null;
+            try
+            {
+                // Creating Connection  
+                con = new SqlConnection(connectionString);
+                // writing sql query  
+                SqlCommand cm = new SqlCommand("select * from Student", con);
+                // Opening Connection  
+                con.Open();
+                SqlDataReader rdr = cm.ExecuteReader();
+                while (rdr.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["name"].ToString();
+                    student.city = rdr["city"].ToString();
+                    studentList.Add(student);
+                }
+                Console.WriteLine("Table created Successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong." + e);
+            }
+            // Closing the connection  
+            finally
+            {
+                con.Close();
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(studentList));
+            //return studentList;
+        }
+        [WebMethod]
+        public void GetStudent(int id)
+        {
+            Student student = new Student();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new
+                    SqlCommand("Select * from Student where id = @id", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@id",
+                    Value = id
+                };
+
+                cmd.Parameters.Add(param);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student.Id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["name"].ToString();
+                    student.city = rdr["city"].ToString();
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(student));
+        }
+        [WebMethod]
+        public void GetStudentsByName(string name)
+        {
+            Student student = new Student();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new
+                    SqlCommand("Select * from Student where name like @name", con);
+
+                SqlParameter param = new SqlParameter()
+                {
+                    ParameterName = "@name",
+                    Value = name + "%"
+                };
+
+                cmd.Parameters.Add(param);
+
+                con.Open();
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    student.Id = Convert.ToInt32(rdr["Id"]);
+                    student.name = rdr["name"].ToString();
+                    student.city = rdr["city"].ToString();
+                }
+            }
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(student));
+        }
+    }
+}
